951a5a07b4a25cb36202d7d62812cc95d4187f1e
Alandis Seals <1seals.seals@gmail.com>
2025-07-06 23:59:37 -0500

    sets up the reverse link so each post “has many” comments

--- backend/database/models/post.js
+++ backend/database/models/post.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Post extends Model {
     /**
@@ -10,15 +8,21 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Post.hasMany(models.Comment, {
+        foreignKey: "postId",
+        onDelete: "CASCADE",
+      });
     }
   }
-  Post.init({
-    title: DataTypes.STRING,
-    content: DataTypes.TEXT
-  }, {
-    sequelize,
-    modelName: 'Post',
-  });
+  Post.init(
+    {
+      title: DataTypes.STRING,
+      content: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: "Post",
+    }
+  );
   return Post;
 };
